7e1a28dbb0a4dd2fcd153116ecea801fa5023b2f
onlyphuoc <44260421+onlyphuoc@users.noreply.github.com>
2018-12-26 15:38:03 +0700

    đọc file từ txt
    đã làm đọc file và login

---
+++ QLLS/frmLogin.cs
@@ -0,0 +1,64 @@
+﻿using QLLS.DAO;
+using QLLS.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLLS
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+            btnDangNhap.Enter += (send, e) => DangNhap();
+            btnDangNhap.Click += (send, e) => DangNhap();
+          
+
+
+        }
+
+      
+
+        private void DangNhap()
+        {
+            string userName = txtTaiKhoan.Text;
+            string passWord = txtMatKhau.Text;
+            if (Login(userName, passWord))
+            {
+
+                frmMain f = new frmMain();
+                f.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("Sai tên tài khoản hoặc mật khẩu!");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        bool Login(string userName, string passWord)
+        {
+            return AccountDAO.Instance.Login(userName, passWord);
+        }
+
+        private void txtMatKhau_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar== Convert.ToChar(Keys.Enter))
+            {
+                DangNhap();
+            }
+        }
+    }
+}
---
+++ QLLS/frmMain.cs
@@ -0,0 +1,109 @@
+﻿using DevComponents.DotNetBar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLLS
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        #region AddNewControlBoxItem
+
+        /// <summary>
+        /// Adds the "New" ControlBox item
+        /// </summary>
+        private void AddNewControlBoxItem()
+        {
+            // Create a new button and add it to the ControlBox
+
+
+
+            ButtonItem bi = new ButtonItem();
+
+           
+            bi.CustomColorName = "ControlBoxColorTable";
+            bi.Click += AddTabClick;
+           
+        }
+
+
+        #endregion
+
+       
+
+        /// <summary>
+        /// Handles "Add Tab" ControlBox selections
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void AddTabClick(object sender, System.EventArgs e)
+        {
+            
+
+            SuperTabItem tab = stcMain.CreateTab("s");
+
+            tab.ImageIndex = 0;
+
+            
+        }
+
+        private void btnCauHinh_Click(object sender, EventArgs e)
+        {
+
+            //SuperTabItem tab = stcMain.CreateTab("Cấu hình");
+            //tab.ImageIndex = 0;
+            stcMain.Visible = true;
+
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            string duongdan = txtChonFile.Text.Trim() + "";
+            if (!string.IsNullOrWhiteSpace(duongdan))
+            {
+                StreamReader sr = new StreamReader(duongdan);
+                do
+                {
+                    string a = sr.ReadLine();
+                    MessageBox.Show(a);
+                } while (sr.Peek() != -1);
+               
+            }
+            
+        }
+
+
+        private void btnChonFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFile = new OpenFileDialog();
+                openFile.InitialDirectory = @"C:\";
+                openFile.DefaultExt = "txt";
+                openFile.Filter = "txt files (*.txt)|*.txt";
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    txtChonFile.Text = openFile.FileName;
+                }
+            }
+            catch (Exception r)
+            {
+                MessageBox.Show(r+"");
+            }
+
+        }
+    }
+}
